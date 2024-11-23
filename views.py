4dc187074ff30d8a65340532f0959a85afddb476
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2024-11-23 03:18:05 +0200

    Add files via upload

--- views.py
+++ views.py
@@ -1,17 +1,17 @@
-from flask import Blueprint ,render_template, request
+from flask import Blueprint ,render_template, request # type: ignore
 
 views = Blueprint(__name__ ,'views'   )
 
 @views.route('/' )
 def home():
-    return render_template('Terms.html')
+    return render_template('Terms.html' ,heading_h1='Terms and Conditions')
 @views.route('/base' )
 def base():
-    return render_template('base.html', title="000" )
+    return render_template('base.html' )
 
 @views.route('/Privacy-Policy')
 def PrivacyPolicy():
-    return render_template('Privacy-Policy.html')
+    return render_template('Privacy-Policy.html' , heading='Privacy Policy')
 
 @views.route('/contact', methods=['GET', 'POST'])
 def contact():
